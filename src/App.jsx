6ba30b22d9dfb3f8e74124f8537a785c76b55215
shahbazqalander388 <syedshahbaz2004@gmail.com>
2025-09-07 04:02:43 -0700

    weather app using react js

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Weather from "./Weather"
+
+export default function App() {
+  return (
+    <div>
+      <Weather/>
+    </div>
+  )
+}
+
---
+++ src/Weather.jsx
@@ -0,0 +1,36 @@
+import "./Weather.css"; 
+import axios from 'axios'
+import { useState } from 'react'
+
+
+
+export default function () {
+    let [city,setcity]=useState("");
+    let [weather,setweather]=useState("")
+    let chkweather=()=>{
+        axios.get(
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a4cac524c79daa46227ab36a52c7a3e5&units=metric`
+)
+
+  .then(function (response) {
+  
+    setweather(response.data.main.temp);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+    }
+  return (
+    <div className="weather-container">
+      <h3>Weather App</h3>
+      <input type="text" onChange={(e)=>(setcity(e.target.value)
+      )}/>
+      <button onClick={chkweather}>check wearther</button>
+      <h4> <i>Today current Temprature </i> {city}{weather}°C</h4>
+    </div>
+  )
+}
+// first go to npm axios example the go to open weather then go to api in weather app per cuerrnet api
+// mai ayi gai per built in web api ko copy kry per per web api ap per ayi waha sai id copy kry gai
+
+// per hum nai use state bna na hai  
